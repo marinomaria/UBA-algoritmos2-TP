4887f6475e7433e2295da0a9809338aa3d24e635
freire-guido <freireguido@outlook.com>
2022-06-12 21:18:15 -0300

    interfaz servidor

--- tpg2/src/Servidor.h
+++ tpg2/src/Servidor.h
@@ -1,4 +1,17 @@
 #ifndef TP_SIMCITY_SERVIDOR_H
 #define TP_SIMCITY_SERVIDOR_H
 
+#include "SimCity.h"
+#include "string_map.h"
+
+class Servidor {
+public:
+    Servidor();
+    void registrar(Jugador j);
+    void avanzarTurno(Jugador j, set<Nat> casas, set<Nat> comercios);
+    void unir(Jugador j1, Jugador j2);
+private:
+    string_map<SimCity> _trie;
+};
+
 #endif //TP_SIMCITY_SERVIDOR_H
