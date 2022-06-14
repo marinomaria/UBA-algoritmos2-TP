c8ce2acf208f4a22261c649fdd3066e6a8aaa935
María Marino <mariamarino@Marias-MacBook-Air.local>
2022-06-14 18:52:37 -0300

    Agrego funciones unir, registrar y constructor de Servidor

--- tpg2/src/Servidor.cpp
+++ tpg2/src/Servidor.cpp
@@ -5,3 +5,17 @@
 SimCity Servidor::at(Jugador j1) const {
     return _trie.at(j1);
 }
+
+Servidor::Servidor() : _trie() {};
+
+void Servidor::registrar(Jugador j, set<Nat> verticales, set<Nat> horizontales) {
+    pair<string, SimCity> p = make_pair(j, SimCity(Mapa(verticales, horizontales)));
+    _trie.insert(p);
+}
+
+void Servidor::unir(Jugador j1, Jugador j2) {
+    SimCity s1 = _trie.at(j1);
+    SimCity s2 = _trie.at(j2);
+    s1.unir(s2);
+}
+
